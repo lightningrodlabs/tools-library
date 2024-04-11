e3f5105152b44ea41451648b3877d28ed36b02d9
Matthias <tuttiacc@hotmail.com>
2024-04-11 17:56:10 +0200

    added validation for links between Curator entries and Tool entries

--- dnas/tools/zomes/integrity/library/src/curator_to_tools.rs
+++ dnas/tools/zomes/integrity/library/src/curator_to_tools.rs
@@ -1,111 +1,116 @@
 use hdi::prelude::*;
 pub fn validate_create_link_curator_to_tools(
-    _action: CreateLink,
+    action: CreateLink,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    let action_hash = base_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+    let curator_action_hash =
+        base_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let curator_record = must_get_valid_record(curator_action_hash)?;
+    if curator_record.action().author() != &action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a Curator entry can create links to Tool entries".into(),
+        ));
+    }
+    let _curator: crate::Curator = curator_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
-    let action_hash = target_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _tool: crate::Tool = record
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Linked action must reference an entry".to_string()
+        )))?;
+    let tool_action_hash =
+        target_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let tool_record = must_get_valid_record(tool_action_hash)?;
+    let _tool: crate::Tool = tool_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Linked action must reference an entry".to_string()
+        )))?;
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_curator_to_tools(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    action: DeleteLink,
+    original_action: CreateLink,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
+    if action.author != original_action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a link from a Curator to a Tool entry can delete that link."
+                .into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_create_link_tool_to_curators(
-    _action: CreateLink,
+    action: CreateLink,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
     // Check the entry type for the given action hash
-    let action_hash = base_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _tool: crate::Tool = record
+    let tool_action_hash =
+        base_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let tool_record = must_get_valid_record(tool_action_hash)?;
+    let _tool: crate::Tool = tool_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Linked action must reference an entry".to_string()
+        )))?;
     // Check the entry type for the given action hash
-    let action_hash = target_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+    let curator_action_hash =
+        target_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let curator_record = must_get_valid_record(curator_action_hash)?;
+    if curator_record.action().author() != &action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a Curator entry can create links from Tool entries to that Curator entry".into(),
+        ));
+    }
+    let _curator: crate::Curator = curator_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Linked action must reference an entry".to_string()
+        )))?;
     // TODO: add the appropriate validation rules
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_tool_to_curators(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    action: DeleteLink,
+    original_action: CreateLink,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    // TODO: add the appropriate validation rules
+    if action.author != original_action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a link from a Tool entry to a Curator entry can delete that link."
+                .into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
