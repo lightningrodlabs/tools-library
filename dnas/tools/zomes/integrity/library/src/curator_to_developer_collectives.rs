23a0d0f2fc90d15dffaedd5620ca63b89b9a6add
Matthias <tuttiacc@hotmail.com>
2024-04-11 17:49:26 +0200

    added validation from curator to developer collectives links

--- dnas/tools/zomes/integrity/library/src/curator_to_developer_collectives.rs
+++ dnas/tools/zomes/integrity/library/src/curator_to_developer_collectives.rs
@@ -1,31 +1,40 @@
 use hdi::prelude::*;
 pub fn validate_create_link_curator_to_developer_collectives(
-    _action: CreateLink,
+    action: CreateLink,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    let action_hash = base_address
-        .into_action_hash()
-        .ok_or(wasm_error!(WasmErrorInner::Guest(
-            "No action hash associated with link".to_string()
-        )))?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+    let curator_action_hash =
+        base_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let curator_record = must_get_valid_record(curator_action_hash)?;
+
+    if curator_record.action().author() != &action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a Curator entry can create links from that entry to a DeveloperCollective entry"
+                .into(),
+        ));
+    }
+
+    let _curator: crate::Curator = curator_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
         .ok_or(wasm_error!(WasmErrorInner::Guest(
             "Linked action must reference an entry".to_string()
         )))?;
-    let action_hash =
+    let collective_action_hash =
         target_address
             .into_action_hash()
             .ok_or(wasm_error!(WasmErrorInner::Guest(
                 "No action hash associated with link".to_string()
             )))?;
-    let record = must_get_valid_record(action_hash)?;
-    let _developer_collective: crate::DeveloperCollective = record
+    let collective_record = must_get_valid_record(collective_action_hash)?;
+    let _developer_collective: crate::DeveloperCollective = collective_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
@@ -35,28 +44,35 @@ pub fn validate_create_link_curator_to_developer_collectives(
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_curator_to_developer_collectives(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    action: DeleteLink,
+    original_action: CreateLink,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
+    if action.author != original_action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a link from a Curator to a DeveloperCollective can delete that link."
+                .into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_create_link_developer_collective_to_curators(
-    _action: CreateLink,
+    action: CreateLink,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    // Check the entry type for the given action hash
-    let action_hash = base_address
-        .into_action_hash()
-        .ok_or(wasm_error!(WasmErrorInner::Guest(
-            "No action hash associated with link".to_string()
-        )))?;
-    let record = must_get_valid_record(action_hash)?;
-    let _developer_collective: crate::DeveloperCollective = record
+    let collective_action_hash =
+        base_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let collective_record = must_get_valid_record(collective_action_hash)?;
+
+    let _developer_collective: crate::DeveloperCollective = collective_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
@@ -64,30 +80,41 @@ pub fn validate_create_link_developer_collective_to_curators(
             "Linked action must reference an entry".to_string()
         )))?;
     // Check the entry type for the given action hash
-    let action_hash =
+    let curator_action_hash =
         target_address
             .into_action_hash()
             .ok_or(wasm_error!(WasmErrorInner::Guest(
                 "No action hash associated with link".to_string()
             )))?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+    let curator_record = must_get_valid_record(curator_action_hash)?;
+
+    if curator_record.action().author() != &action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a Curator entry can create a link from a DeveloperCollective entry to that Curator entry.".into(),
+        ));
+    }
+
+    let _curator: crate::Curator = curator_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
         .ok_or(wasm_error!(WasmErrorInner::Guest(
             "Linked action must reference an entry".to_string()
         )))?;
-    // TODO: add the appropriate validation rules
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_developer_collective_to_curators(
-    _action: DeleteLink,
-    _original_action: CreateLink,
+    action: DeleteLink,
+    original_action: CreateLink,
     _base: AnyLinkableHash,
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    // TODO: add the appropriate validation rules
+    if action.author != original_action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Only the creator of a link from a Curator to a DeveloperCollective can delete that link."
+                .into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
