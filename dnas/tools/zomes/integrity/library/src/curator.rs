ef114bde2fb6e161d60ec7347d1ab67d0981c9b4
Matthias <tuttiacc@hotmail.com>
2024-04-11 16:58:27 +0200

    added validation for Curator entry

--- dnas/tools/zomes/integrity/library/src/curator.rs
+++ dnas/tools/zomes/integrity/library/src/curator.rs
@@ -16,62 +16,71 @@ pub fn validate_create_curator(
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_update_curator(
-    _action: Update,
+    action: Update,
     _curator: Curator,
-    _original_action: EntryCreationAction,
+    original_action: EntryCreationAction,
     _original_curator: Curator,
 ) -> ExternResult<ValidateCallbackResult> {
+    if &action.author != original_action.author() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Curator entries can only be updated by the agent that created the entry.".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_curator(
-    _action: Delete,
-    _original_action: EntryCreationAction,
+    action: Delete,
+    original_action: EntryCreationAction,
     _original_curator: Curator,
 ) -> ExternResult<ValidateCallbackResult> {
+    if &action.author != original_action.author() {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Curator entries can only be deleted by the agent that created the entry.".into(),
+        ));
+    }
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_create_link_curator_updates(
-    _action: CreateLink,
+    action: CreateLink,
     base_address: AnyLinkableHash,
     target_address: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    let action_hash = base_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+    let curator_action_hash =
+        base_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let curator_record = must_get_valid_record(curator_action_hash)?;
+
+    if curator_record.action().author() != &action.author {
+        return Ok(ValidateCallbackResult::Invalid(
+            "Links to curator entry updates can only be created by the agent that created the curator entry.".into(),
+        ));
+    }
+
+    let _curator: crate::Curator = curator_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
-    let action_hash = target_address
-        .into_action_hash()
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("No action hash associated with link".to_string())
-            ),
-        )?;
-    let record = must_get_valid_record(action_hash)?;
-    let _curator: crate::Curator = record
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Link base of a link to a Curator update must be Curator entry".to_string()
+        )))?;
+    let curator_update_action_hash =
+        target_address
+            .into_action_hash()
+            .ok_or(wasm_error!(WasmErrorInner::Guest(
+                "No action hash associated with link".to_string()
+            )))?;
+    let curator_update_record = must_get_valid_record(curator_update_action_hash)?;
+    let _curator_update: crate::Curator = curator_update_record
         .entry()
         .to_app_option()
         .map_err(|e| wasm_error!(e))?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest("Linked action must reference an entry"
-                .to_string())
-            ),
-        )?;
+        .ok_or(wasm_error!(WasmErrorInner::Guest(
+            "Link target of a link to a Curator update must be a Curator entry".to_string()
+        )))?;
     Ok(ValidateCallbackResult::Valid)
 }
 pub fn validate_delete_link_curator_updates(
@@ -81,9 +90,7 @@ pub fn validate_delete_link_curator_updates(
     _target: AnyLinkableHash,
     _tag: LinkTag,
 ) -> ExternResult<ValidateCallbackResult> {
-    Ok(
-        ValidateCallbackResult::Invalid(
-            String::from("CuratorUpdates links cannot be deleted"),
-        ),
-    )
+    Ok(ValidateCallbackResult::Invalid(String::from(
+        "CuratorUpdates links cannot be deleted",
+    )))
 }
